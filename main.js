e0754eeee4466dd05f0735b0e8758ac75b2e0c3d
Arash Soltani <soltaniworld@gmail.com>
2020-08-31 14:39:38 -0400

    cleaned up code, made it more BEM appropriate. Reused some code from card-footer to replace now-deleted content-footer

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const shareButton = document.getElementById('share-button');
 const shareMedia = document.getElementById('social-media');
 const returnButton = document.getElementById('return-button')
+const activeStateClass = 'card-footer-dark--display'
 
 shareButton.addEventListener('click', () => {
-	shareMedia.classList.toggle('active');
-	shareButton.parentNode.classList.toggle('icon-share-inverse-color');
+	shareMedia.classList.toggle(activeStateClass);
 });
 
 returnButton.addEventListener('click', () => {
-	if (shareMedia.classList.contains('active')) {
-		shareMedia.classList.remove('active');
+	if (shareMedia.classList.contains(activeStateClass)) {
+		shareMedia.classList.remove(activeStateClass);
 	}
 });
